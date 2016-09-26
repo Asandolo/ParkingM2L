376bdf4e8c3081494c424aa4098828223cca8a80
Vkasperski <victor.kasperski@laposte.net>
2016-09-26 16:53:24 +0200

    lol

--- place_dispo.php
+++ place_dispo.php
@@ -7,17 +7,9 @@ include("includes/pages/header.php");
 			<div class="col-md-12 black">
 				<p>PLACES DISPONNIBLES</p>
 				<?php
-					$checkPlaces = $bdd->query("SELECT id_place,civilite_membre,nom_membre,prenom_membre FROM MEMBRE,RESERVER,WHERE 'RESERVER.id_membre' = 'MEMBRES.id_membre'");
-					while ($donnees = $checkPlaces->fetch()) 
-					{
-						?>
-						<td name="Num. place">
-							<tr>
-								<?php echo $donnees['id_membre'] ?>
-							</tr>
-						</td>
-						<?php	
-					}
+					$aujourdhui = date("y-m-d");
+					$CheckPlace = $bdd->prepare("SELECT * FROM RESERVER WHERE ?")
+
 				?>
 			</div>
 		</div>
