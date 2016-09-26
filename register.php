347806296334ba14e5d9c57fdb8f3aa839c49fb0
Asandolo <asandolo3@gmail.com>
2016-09-26 16:31:29 +0200

    LE PUTAIN DE D'ENCULER DE FILS DE PUTE SUCEUR DE BITE ET VIOLEUR DE MOUTON DE REGISTER SA MERE

--- register.php
+++ register.php
@@ -39,15 +39,11 @@
 
       $verif->closeCursor();
 
-      $r = $bdd->prepare("INSERT INTO membre (mail_membre) VALUES(?)");
-      $r->execute(array($mail));
+      $r = $bdd->prepare("INSERT INTO `membre` (`id_membre`, `mail_membre`, `psw_membre`, `civilite_membre`, `nom_membre`, `prenom_membre`, `date_naiss_membre`, `adRue_membre`, `adCP_membre`, `adVille_membre`, `rang`, `valide_membre`, `admin_membre`) VALUES (NULL, ?, ?, ?, ?, ?, ?, ?, ?, ?, '0', '0', '0')");
+      $r->execute(array($mail,$hashpsw,$civilite,$nom,$prenom,$date_naiss,$rue,$cp,$ville));
 
+      $ok = "ok";
 
-      echo "<pre>";
-      print_r($r);
-      echo "</pre>";
-
-      $ok = 'ok';
 
       $bdd = null;
     }
