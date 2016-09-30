6089ae653e74fb653f93cf0035bb2364a0172824
Vkasperski <victor.kasperski@laposte.net>
2016-09-30 13:11:02 +0200

    requettes terminées

--- place_dispo.php
+++ place_dispo.php
@@ -3,42 +3,57 @@ $titre = "places disponibles";
 include("includes/pages/header.php");
 ?>
 
-		<div class="row">
-			<div class="col-md-12 black">
-				<p>PLACES DISPONNIBLES</p>
-				<?php
-					$aujourdhui = date("y-m-d");
-					$CheckPlace = $bdd->prepare("SELECT num_place FROM PLACE ");
-					$CheckPlace->execute();
-					$i = 1;
-					while($donnee = $CheckPlace->fetch())
-					{
-						$place[$i]=$donnee['num_place'];
-						$i++;
-					}
+<div class="row">
+	<div class="col-md-12 black">
+		<p><center>PLACES DISPONNIBLES</center></p>
+	</div>		
+</div>
+<div class="row">
+	<div class="col-md-12 black">
+		<p>
+		<center>
+			<?php
+				$aujourdhui = date("y-m-d");
+//-----------------------------
+//SELECTION DES PLACES RESERVE |
+//-----------------------------	
 
-					$k=1;
-					while ($k< $i) 
-					{
-						echo $place[$k];
-					}
-					
-					$ajd = date("Y-m-d"); 
-					$tsajd = strtotime($ajd);
 
-					$places = $bdd->prepare("SELECT * FROM `reserver`, `place` WHERE date_debut_periode > ? AND date_fin_periode < ? AND `reserver`.`id_place` = `place`.`id_place`");
-					$places->execute(array($ajd,$ajd));
-					$j = 1;
-					while ($doneeDispo = $places->fetch()) 
-					{
-						$placeDispo[$j] = $donneeDispo['num_place'];
-						$j++;
-						echo "<pre>";
-						echo $placeDispo[$j];
-						echo "</pre>";
-					}
-				?>
-			</div>
-		</div>
+				$CheckPlace = $bdd->prepare("SELECT num_place FROM PLACE,RESERVER WHERE PLACE.id_place = RESERVER.id_place AND date_fin_periode>= ? AND date_debut_periode <= ?");
+					$CheckPlace->execute(array($aujourdhui, $aujourdhui));
+				$i = 1;
+				while($donnee = $CheckPlace->fetch())
+				{
+					$place[$i]=$donnee['num_place'];
+					echo $place[$i]."</br>";
+					$i++;
+				}
+
+
+//--------------------------------------------
+// SELECTION DE TOUTES LES PALCES DE PARKING |
+//--------------------------------------------
+				$tsajd = strtotime($aujourdhui);
+				
+				$places = $bdd->prepare("SELECT num_place FROM PLACE");
+				$places->execute();
+				
+				$j = 1;
+				while ($donneeDispo = $places->fetch()) 
+				{
+					$placeDispo[$j] = $donneeDispo['num_place'];
+					echo $placeDispo[$j]."</br>";
+					$j++;
+				}
+
+//----------------------------------------------------------------
+//MAGNIPULATION DES TABLEAUX POUR RESORTIR LES PLACES DISPONIBLES |
+//----------------------------------------------------------------
+
+			?>
+		</center>
+		</p>
+	</div>
+</div>
 
 <?php include("includes/pages/footer.php"); ?>
