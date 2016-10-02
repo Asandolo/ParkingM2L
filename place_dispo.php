3f3a40a4be5b8f6a1e8a901e475fc47c1ead2ec8
Vkasperski <victor.kasperski@laposte.net>
2016-10-02 19:10:04 +0200

    lol

--- place_dispo.php
+++ place_dispo.php
@@ -11,96 +11,129 @@ include("includes/pages/header.php");
 <div class="row">
 	<div class="col-md-12 black">
 		<p>
-		<center>
-			<?php
-				$aujourdhui = date("y-m-d");
+			<center>
+				<?php
+					if (isset($_POST['reserver'])) 
+					{
+						$aujourdhui = date("y-m-d");
 //-----------------------------
 //SELECTION DES PLACES RESERVE |
 //-----------------------------	
 
-				$CheckPlace = $bdd->prepare("SELECT num_place FROM PLACE,RESERVER WHERE PLACE.id_place = RESERVER.id_place AND date_fin_periode>= ? AND date_debut_periode <= ?");
-					$CheckPlace->execute(array($aujourdhui, $aujourdhui));
-				$i = 0;
-				while($donnee = $CheckPlace->fetch())
-				{
-					$place[$i]=$donnee['num_place'];
-					//echo $place[$i]."</br>";
-					$i++;
-				}
+						$CheckPlace = $bdd->prepare("SELECT num_place FROM PLACE,RESERVER WHERE PLACE.id_place = RESERVER.id_place AND date_fin_periode>= ? AND date_debut_periode <= ?");
+						$CheckPlace->execute(array($aujourdhui, $aujourdhui));
+						$i = 0;
+						while($donnee = $CheckPlace->fetch())
+						{
+							$place[$i]=$donnee['num_place'];
+							$i++;
+						}
 
 
 //--------------------------------------------
 // SELECTION DE TOUTES LES PALCES DE PARKING |
 //--------------------------------------------
 				
-				$tsajd = strtotime($aujourdhui);
-				
-				$places = $bdd->prepare("SELECT num_place FROM PLACE");
-				$places->execute();
-				
-				$j = 0;
-				while ($donneeDispo = $places->fetch()) 
-				{
-					$placeDispo[$j] = $donneeDispo['num_place'];
-					//echo $placeDispo[$j]."</br>";
-					$j++;
-				}
+						$tsajd = strtotime($aujourdhui);
+					
+						$places = $bdd->prepare("SELECT num_place FROM PLACE");
+						$places->execute();
+					
+						$j = 0;
+						while ($donneeDispo = $places->fetch()) 
+						{
+							$placeDispo[$j] = $donneeDispo['num_place'];
+							$j++;
+						}
 
 //----------------------------------------------------------------
 //MAGNIPULATION DES TABLEAUX POUR RESORTIR LES PLACES DISPONIBLES |
 //----------------------------------------------------------------
-				// $place[0] = 0;
-				// $placeDispo[0]=0;
-				// $k=1;
-				// $l=1;
-				// $trieur = 1;
-				// for($k=1;$k<=$i;$k++)
-				// {
-				// 	for($l=1;$l<=$j;$l++)
-				// 	{
-				// 		if($place[$l] == $placeDispo[$k])
-				// 		{
-				// 			$placeDispo[$l] = null;
-				// 			for($trieur=$l;$trieur<$l;$trieur++)
-				// 			{
-				// 				$placeDispo[$trieur]=$placeDispo[$trieur+1];
-				// 			}
-				// 		}	
 
-				// 	}
-				// }
-
-				//La fonction que tu cherchais
-				if(empty($place)){
-					echo "VIIIIIIIIIIIIIIIIDE";
-				}
-
-
-				for ($k=0; $k <=count($place)-1 ; $k++) 
-				{ 
-					for ($l=0; $l <=count($placeDispo)-1 ; $l++) 
-					{ 
-						if ($place[$k] == $placeDispo[$l]) 
+						if(empty($place))
 						{
-							$placeDispo[$l]=null;
-							for($increment = $l ; $increment<count($placeDispo)-1 ; $increment++) 
+						
+						}
+						else
+						{
+							for ($k=0; $k <=count($place)-1 ; $k++) 
 							{ 
-								$placeDispo[$increment] = $placeDispo[$increment+1];
+								for ($l=0; $l <=count($placeDispo)-1 ; $l++) 
+								{ 
+									if ($place[$k] == $placeDispo[$l]) 
+									{
+										$placeDispo[$l]=null;
+										for($increment = $l ; $increment<count($placeDispo)-1 ; $increment++) 
+										{ 
+											$placeDispo[$increment] = $placeDispo[$increment+1];
+										}	
+									}
+								}
 							}
-							
+						}
+
+						for($k=0;$k<$j;$k++)
+						{
+							echo $placeDispo[$k]."</br>";
 						}
 					}
-				}
-
-
-				for($k=0;$k<$j;$k++)
-				{
-					echo $placeDispo[$k]."</br>";
-				}
-			?>
-		</center>
+				?>
+			</center>
 		</p>
 	</div>
 </div>
 
+
+
+
+<div class="row">
+<div class="col-md-12 black">
+	<p>
+	<center>
+		<form method="POST">
+			<label style="color : #0beee8;">Jour</label><select name="jour" required="">
+   		 <?php
+      		for ($i=1;$i<=31;$i++) {
+   		 ?>
+    	<option style="color : black;">
+        	<?php
+          		echo $i;
+       		 ?>  
+    	</option>
+   		<?php
+      		}
+   		?>
+    	</select>
+    	<label style="color : #0beee8;">Mois</label><select name="mois" required="">
+    	<?php
+      		for ($i=1;$i<=12;$i++) {
+   		?>
+    	<option>
+        <?php
+          	echo $i;
+        ?>    
+    	</option>
+    	<?php
+      		}
+    	?>
+    	</select>
+    	<label style="color : #0beee8;">Année</label><select name="annee" required="">
+    	<?php
+      		for ($i=Date("Y");$i<=Date("Y")+3;$i++) {
+    	?>
+    	<option>
+        	<?php
+         		echo $i;
+        	?>    
+    	</option>
+    	<?php
+      		}
+    	?>
+    	</select></br>
+    	<input type="submit" name="reserver" class="btn btn-info">
+    	</form>  
+	</center>
+	</p>
+</div>
+</div>
 <?php include("includes/pages/footer.php"); ?>
