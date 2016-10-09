46364191b768f7ea3e547e1e7e3c3d94f5dc5fc7
Vkasperski <victor.kasperski@laposte.net>
2016-10-09 21:32:49 +0200

    fini ?

--- place_dispo.php
+++ place_dispo.php
@@ -19,6 +19,8 @@ while ($donneCheckHaveRang = $checkHaveRang->fetch())
   $rangUser = $donneCheckHaveRang['rang'];
 }
 
+
+$havePlace=0;
 if($countHavePlace>0)
 {
   while ($donneCheckHavePlace = $checkHavePlace->fetch()) 
@@ -26,47 +28,50 @@ if($countHavePlace>0)
     if(($donneCheckHavePlace['date_debut_periode']<=$aujourdhui) && ($donneCheckHavePlace['date_fin_periode']>=$aujourdhui))
     {
       $placeUser = $donneCheckHavePlace['num_place'];    
+      $havePlace = 1;
       ?>
-      <div class="row">
-        <div class="col-md-12 black">
-          <p><center><h2><?php echo "Vous avez la place numéro ".$placeUser; ?></h2></center></p>
-        </div>
-      </div> 
+        <div class="row">
+          <div class="col-md-12 black">
+            <p><center><h2><?php echo "Vous avez la place numéro ".$placeUser; ?></h2></center></p>
+          </div>
+        </div> 
       <?php
     }
   }  
 }
+
+
 elseif ($rangUser>0) 
 {
-
   ?>
   <div class="row">
     <div class="col-md-12 black">
-      <p><center><h2><?php echo "Vous avez le rang numéro ".$rangUser; ?></h2></center></p>
+    <p><center><h2><?php echo "Vous avez le rang numéro ".$rangUser; ?></h2></center></p>
     </div>
   </div> 
   <?php
 }
-elseif ($countHavePlace == 0 && $rangUser == 0) 
+
+
+elseif ($havePlace == 0 && $rangUser == 0) 
 {
   if (isset($_POST['reserver'])) 
   {
+      $j_deb = $_POST['jour_deb'];
+      $m_deb = $_POST['mois_deb'];
+      $a_deb = $_POST['annee_deb'];
+      $debut_resa = $a_deb."-".$m_deb."-".$j_deb;
+      $j_fin = $_POST['jour_fin'];
+      $m_fin = $_POST['mois_fin'];
+      $a_fin = $_POST['annee_fin'];
+      $fin_resa = $a_fin."-".$m_fin."-".$j_fin;
 
-    $j_deb = $_POST['jour_deb'];
-    $m_deb = $_POST['mois_deb'];
-    $a_deb = $_POST['annee_deb'];
-    $debut_resa = $a_deb."-".$m_deb."-".$j_deb;
-    $j_fin = $_POST['jour_fin'];
-    $m_fin = $_POST['mois_fin'];
-    $a_fin = $_POST['annee_fin'];
-    $fin_resa = $a_fin."-".$m_fin."-".$j_fin;
-
-    if ($debut_resa<=$fin_resa && $debut_resa>=$aujourdhui) 
+    if ($debut_resa<=$fin_resa) 
     {
 //-----------------------------
 //SELECTION DES PLACES RESERVE |
 //-----------------------------	
-
+      echo"yolo"; 
       $CheckPlace = $bdd->prepare("SELECT num_place,date_debut_periode,date_fin_periode FROM PLACE,RESERVER WHERE PLACE.id_place = RESERVER.id_place");
       $CheckPlace->execute(array($fin_resa,$fin_resa,$debut_resa,$debut_resa,$debut_resa,$fin_resa));
       $i = 0;
@@ -116,7 +121,23 @@ elseif ($countHavePlace == 0 && $rangUser == 0)
 
       if(empty($place))
       {
-
+        $checkPeriode = $bdd->prepare("SELECT* FROM PERIODE");
+        $checkPeriode ->execute();
+        $isPeriode= 0;
+        while ($donneeCheckPer = $checkPeriode->fetch()) 
+        {
+          if($donneeCheckPer['date_debut_periode'] == $debut_resa)
+          {
+            $isPeriode=1;
+          }
+        }
+        if($isPeriode!=1)
+        {
+          $insertPer = $bdd->prepare("INSERT INTO PERIODE VALUES (?)");
+          $insertPer->execute(array($debut_resa));
+        }
+        $insertResa = $bdd->prepare("INSERT INTO RESERVER VALUES (?,?,?,?)");
+        $insertResa->execute(array($fin_resa,$user['id_membre'],$placeDispo[0],$debut_resa));
       }
       else
       {
@@ -126,108 +147,140 @@ elseif ($countHavePlace == 0 && $rangUser == 0)
           { 
             if ($place[$k] == $placeDispo[$l]) 
             {
-              $placeDispo[$l]=null;
               for($increment = $l ; $increment<count($placeDispo)-1 ; $increment++) 
               { 
-               $placeDispo[$increment] = $placeDispo[$increment+1];
-             }	
-             array_pop($placeDispo);
-           }
-         }
+                 $placeDispo[$increment] = $placeDispo[$increment+1];
+              }	
+               array_pop($placeDispo);
+            }
+          }
+        }
        }
-     }
-
-     for($k=0;$k<count($placeDispo);$k++)
-     {
-      echo $placeDispo[$k]."</br>";
-    }
-  }
+    
 //-----------------------------------------------------------
 // SI IL N'Y A PAS DE PLACE DISPONNIBLE ON MET DANS LE RANG  |
 //-----------------------------------------------------------
 
-  if (empty($placeDispo)) 
-  {
-    $countRang = $bdd->prepare("SELECT rang FROM MEMBRE ORDER BY rang");
-    $countRang->execute();
-    $attribution=0;
-    while ($donneeCRang = $countRang->fetch()) 
-    {
-      $attribution = $donneeCRang['rang']+1;
-    }
-    echo $attribution;
-
-    $attribRang = $bdd->prepare("UPDATE MEMBRE SET rang = ? WHERE id_membre= ?")  ;
-    $attribRang -> execute(array($attribution,$user['id_membre']));
-  }
+      if (empty($placeDispo)) 
+      {
+        $countRang = $bdd->prepare("SELECT rang FROM MEMBRE ORDER BY rang");
+        $countRang->execute();
+        $attribution=0;
+        while ($donneeCRang = $countRang->fetch()) 
+        {
+          $attribution = $donneeCRang['rang']+1;
+        }
+        echo $attribution;
+      
+        $attribRang = $bdd->prepare("UPDATE MEMBRE SET rang = ? WHERE id_membre= ?")  ;
+        $attribRang -> execute(array($attribution,$user['id_membre']));
+      }
 
 //--------------------------
 // 
 //--------------------------
 
-  else
-  {
-    $checkPeriode = $bdd->prepare("SELECT* FROM PERIODE");
-    $checkPeriode ->execute();
-    $isPeriode= 0;
-    while ($donneeCheckPer = $checkPeriode->fetch()) 
-    {
-      if($donneeCheckPer['date_debut_periode'] == $debut_resa)
+      else
       {
-        $isPeriode=1;
+        $checkPeriode = $bdd->prepare("SELECT* FROM PERIODE");
+        $checkPeriode ->execute();
+        $isPeriode= 0;
+        while ($donneeCheckPer = $checkPeriode->fetch()) 
+        {
+          if($donneeCheckPer['date_debut_periode'] == $debut_resa)
+          {
+            $isPeriode=1;
+          }
+        }
+        if($isPeriode!=1)
+        {
+          $insertPer = $bdd->prepare("INSERT INTO PERIODE VALUES (?)");
+          $insertPer->execute(array($debut_resa));
+        }
+        $insertResa = $bdd->prepare("INSERT INTO RESERVER VALUES (?,?,?,?)");
+        $insertResa->execute(array($fin_resa,$user['id_membre'],$placeDispo[0],$debut_resa));
       }
-    }
-    if($isPeriode=1)
+    } 
+    else
     {
-      $insertPer = $bdd->prepare("INSERT INTO PERIODE VALUES (?)");
-      $insertPer->execute(array($debut_resa));
+      echo "Erreur selections des dates, la date de début doits être avant la date de fin et doit se situer après la date d'aujourdhui"; 
     }
-    $insertResa = $bdd->prepare("INSERT INTO RESERVER VALUES (?,?,?,?)");
-    $insertResa->execute(array($fin_resa,$user['id_membre'],$placeDispo[0],$debut_resa));
   }
-}
-$j_deb = $_POST['jour_deb'];
-$m_deb = $_POST['mois_deb'];
-$a_deb = $_POST['annee_deb'];
-$debut_resa = $a_deb."-".$m_deb."-".$j_deb;
-$j_fin = $_POST['jour_fin'];
-$m_fin = $_POST['mois_fin'];
-$a_fin = $_POST['annee_fin'];
-$fin_resa = $a_fin."-".$m_fin."-".$j_fin;
-elseif($debut_resa>$fin_resa)
-{
-  echo "Erreur selections des dates, la date de début doits être avant la date de   fin et doit se situer après la date d'aujourdhui"; 
-}
-?>
-
-<div class="row">
- <div class="col-md-12 black">
-  <p><center>PLACES DISPONNIBLES</center></p>
-</div>		
-
-
-
-<div class="col-md-12 black">
- <p>
-   <center>
-    <form method="POST">
-     DATE DE DEBUT : </br></br>
-     <label style="color : #0beee8;">Jour</label><select name="jour_deb" required="" style="color : black;">
-     <?php
-     for ($i=1;$i<=31;$i++) {
-      ?>
-      <option>
-       <?php
-       echo $i;
-       ?>  
-     </option>
-     <?php
-   }
+  ?>
+  
+  <div class="row">
+  	<div class="col-md-12 black">
+  		<p><center>PLACES DISPONNIBLES</center></p>
+  	</div>		
+  
+  
+  
+    <div class="col-md-12 black">
+     <p>
+       <center>
+        <form method="POST">
+         DATE DE DEBUT : </br></br>
+         <label style="color : #0beee8;">Jour</label><select name="jour_deb" required="" style="color : black;">
+         <?php
+         for ($i=1;$i<=31;$i++) {
+          ?>
+          <option>
+          	<?php
+            echo $i;
+            ?>  
+          </option>
+          <?php
+        }
+        ?>
+      </select>
+      <label style="color : #0beee8;">Mois</label><select name="mois_deb" required="" style="color : black;">
+      <?php
+      for ($i=1;$i<=12;$i++) {
+       ?>
+       <option>
+        <?php
+        echo $i;
+        ?>    
+      </option>
+      <?php
+    }
+    ?>
+  </select>
+  <label style="color : #0beee8;">Année</label><select name="annee_deb" required="" style="color : black;">
+  <?php
+  for ($i=Date("Y");$i<=Date("Y")+3;$i++) 
+  {
    ?>
- </select>
- <label style="color : #0beee8;">Mois</label><select name="mois_deb" required="" style="color : black;">
- <?php
- for ($i=1;$i<=12;$i++) {
+   <option>
+     <?php
+     echo $i;
+     ?>    
+   </option>
+   <?php
+  }
+  ?>
+  </select></br>
+  
+  
+  DATE DE FIN : </br></br>
+  <label style="color : #0beee8;">Jour</label><select name="jour_fin" required="" style="color : black;">
+  <?php
+  for ($i=1;$i<=31;$i++) 
+  {
+    ?>
+    <option>
+     <?php
+     echo $i;
+     ?>  
+   </option>
+   <?php
+  }
+  ?>
+  </select>
+  <label style="color : #0beee8;">Mois</label><select name="mois_fin" required="" style="color : black;">
+  <?php
+  for ($i=1;$i<=12;$i++) 
+  {
    ?>
    <option>
     <?php
@@ -235,74 +288,30 @@ elseif($debut_resa>$fin_resa)
     ?>    
   </option>
   <?php
-}
-?>
-</select>
-<label style="color : #0beee8;">Année</label><select name="annee_deb" required="" style="color : black;">
-<?php
-for ($i=Date("Y");$i<=Date("Y")+3;$i++) 
-{
- ?>
- <option>
-   <?php
-   echo $i;
-   ?>    
- </option>
- <?php
-}
-?>
-</select></br>
-
-
-DATE DE FIN : </br></br>
-<label style="color : #0beee8;">Jour</label><select name="jour_fin" required="" style="color : black;">
-<?php
-for ($i=1;$i<=31;$i++) 
-{
+  }
   ?>
-  <option>
-   <?php
-   echo $i;
-   ?>  
- </option>
- <?php
-}
-?>
-</select>
-<label style="color : #0beee8;">Mois</label><select name="mois_fin" required="" style="color : black;">
-<?php
-for ($i=1;$i<=12;$i++) 
-{
- ?>
- <option>
+  </select>
+  <label style="color : #0beee8;">Année</label><select name="annee_fin" required="" style="color : black;">
   <?php
-  echo $i;
-  ?>    
-</option>
-<?php
-}
-?>
-</select>
-<label style="color : #0beee8;">Année</label><select name="annee_fin" required="" style="color : black;">
-<?php
-for ($i=Date("Y");$i<=Date("Y")+3;$i++) 
-{
- ?>
- <option>
+  for ($i=Date("Y");$i<=Date("Y")+3;$i++) 
+  {
+   ?>
+   <option>
+     <?php
+     echo $i;
+     ?>    
+   </option>
    <?php
-   echo $i;
-   ?>    
- </option>
- <?php
+  }
+  ?>
+  </select></br>
+  <input type="submit" name="reserver" class="btn btn-info">
+  </form>  
+  </center>
+  </p>
+  </div>
+  </div>
+  <?php  
 }
 ?>
-</select></br>
-<input type="submit" name="reserver" class="btn btn-info">
-</form>  
-</center>
-</p>
-</div>
-</div>
-<?php  
-}
-include("includes/pages/footer.php"); ?>
+<?php include("includes/pages/footer.php"); ?>
