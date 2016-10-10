a13178c8715a5a948183a7117650f65de3890a42
Asandolo <asandolo3@gmail.com>
2016-10-10 13:27:05 +0200

    ss

--- register.php
+++ register.php
@@ -46,7 +46,7 @@ if(isset($_POST['enregistrer']))
     $r = $bdd->prepare("INSERT INTO `membre` (`id_membre`, `mail_membre`, `psw_membre`, `civilite_membre`, `nom_membre`, `prenom_membre`, `date_naiss_membre`, `adRue_membre`, `adCP_membre`, `adVille_membre`, `rang`, `valide_membre`, `admin_membre`) VALUES (NULL, ?, ?, ?, ?, ?, ?, ?, ?, ?, '0', '0', '0')");
     $r->execute(array($mail,$hashpsw,$civilite,$nom,$prenom,$date_naiss,$rue,$cp,$ville));
 
-    $ok = "Vos informations on biens été enreistrés";
+    $ok = "Vos informations on bien été enreistrés";
 
 
     $bdd = null;
